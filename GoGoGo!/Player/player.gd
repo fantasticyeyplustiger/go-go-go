748c5f8c2a95aa6bc9b7cefa03badc24f88c4b1e
fantasticyeyplustiger <143634964+fantasticyeyplustiger@users.noreply.github.com>
2024-05-14 13:25:11 -0400

    player can move

--- GoGoGo!/Player/player.gd
+++ GoGoGo!/Player/player.gd
@@ -1,13 +1,34 @@
-extends Node2D
+extends CharacterBody2D
 
+var tile_size = 256
+var inputs = {"right": Vector2.RIGHT, "left": Vector2.LEFT,
+		"up": Vector2.UP, "down": Vector2.DOWN}
 
-# Called when the node enters the scene tree for the first time.
+'''
+-- READY --
+- sets the position of the player to the center
+- called once player is instanced
+'''
 func _ready():
-	pass # Replace with function body.
+	
+	position = position.snapped(Vector2.ONE * tile_size)
+	position += Vector2.ONE * (tile_size/2)
+	
 
+func _unhandled_input(event):
+	for dir in inputs.keys():
+		if event.is_action_pressed(dir):
+			move(dir)
+
+func move(dir):
+	position += inputs[dir] * tile_size
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+	
+	
+	
+	
 	
 	
 	
