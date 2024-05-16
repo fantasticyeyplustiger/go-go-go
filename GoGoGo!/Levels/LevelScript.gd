05df684baac3a9feeee51b4ef8073a8042e016f5
fantasticyeyplustiger <143634964+fantasticyeyplustiger@users.noreply.github.com>
2024-05-16 14:10:46 -0400

    bunch of stuff

--- GoGoGo!/Levels/LevelScript.gd
+++ GoGoGo!/Levels/LevelScript.gd
@@ -4,22 +4,35 @@ extends Node2D
 @export var columns = 0
 @export var beats_per_minute = 0
 
+enum obstacles {NONE, BOULDER, ROCK_PELLET, STEEL_BALL, IRON_PELLET}
+
 var rng = RandomNumberGenerator.new()
 var wave = 0
-var attack_rows = 0
-var attack_columns = 0
+var tile_size = 256
+
+var attack_right_rows = []
+var attack_left_rows = []
+var attack_up_columns = []
+var attack_down_columns = []
 
 #region obstacle variables
-var bouler = preload("res://Obstacles/Boulder.tscn")
-var steel_ball = preload("res://Obstacles/SteelBall.tscn")
-var rock_pellet = preload("res://Obstacles/RockPellet.tscn")
-var iron_pellet = preload("res://Obstacles/IronPellet.tscn")
+var bouler = preload("res://Obstacles/Boulder.tscn").instantiate()
+var steel_ball = preload("res://Obstacles/SteelBall.tscn").instantiate()
+var rock_pellet = preload("res://Obstacles/RockPellet.tscn").instantiate()
+var iron_pellet = preload("res://Obstacles/IronPellet.tscn").instantiate()
 #endregion
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
+	attack_right_rows.resize(rows)
+	attack_left_rows.resize(rows)
+	attack_up_columns.resize(columns)
+	attack_down_columns.resize(columns)
+	
+	
 	$Timer.wait_time = 2
 	pass # Replace with function body.
 
@@ -42,13 +55,25 @@ func _process(delta):
 func get_random_obstacles(wave, attack_rows, attack_columns):
 	var obstacles = rng.randi_range(1, 3)
 	obstacles = obstacles * (wave / 5)
+
+func attack():
+	var amount_of_attacks = randi_range(0, 10)
 	
+	for attacks in amount_of_attacks:
+		var left_right_up_or_down = randi_range(0, 1)
+		var vertical_or_horizontal = randi_range(0, 1)
+		if vertical_or_horizontal == 0:
+			
+			pass
 
-#func get_random_row_and_column(wave):
-	#attack_rows = rng.randi_range(0, rows)
-	#attack_columns = rng.randi_range(0, columns)
+func get_random_row_and_column(direction):
+	if direction == 0:
+		# attack_rows = rng.randi_range(0, rows - 1)
+		pass
+	elif direction == 1:
+		# attack_columns = rng.randi_range(0, columns - 1)
+		pass
 	#pass
-
 
 '''
 -- ON RHYTHM --
