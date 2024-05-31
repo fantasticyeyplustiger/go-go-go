45a226c01e0872a968f920b151f9de85fe1ef17e
fantasticyeyplustiger <143634964+fantasticyeyplustiger@users.noreply.github.com>
2024-05-31 11:54:44 -0400

    better readability

--- GoGoGo!/Levels/LevelScript.gd
+++ GoGoGo!/Levels/LevelScript.gd
@@ -39,13 +39,16 @@ func _ready():
 	data._add_event(2000,0,Vector2(2,0))
 	data._add_event(3000,0,Vector2(0,2))
 	data._add_event(4000,0,Vector2(1,6))
+	
 	#data._load_from_string("{\"events\":[{\"timing\":100,\"type\":0}]}")
 	bpm /= 4
 	beatLength = (60/bpm) * 1000
+	
 	for x in gridSize:
 		spawners.append([])
 		for y in gridSize:
 			spawners[x].append(Vector2(128+(x-1)*256,-128+(6-y)*-256))
+			
 	gridSize-=1
 
 '''
@@ -60,6 +63,7 @@ func _physics_process(_delta):
 	Globals.songMilliseconds = audio.get_playback_position()*1000
 	var events = data.events
 	var loopRange = range(0,events.size())
+	
 	for i in loopRange:
 		if(events[i].timing <= Globals.songMilliseconds&&!events[i].activated):
 			# all the code below spawns a boulder, change this to a function once we add more obstacles!
@@ -70,7 +74,9 @@ func _physics_process(_delta):
 		# on_rhythm()
 
 func _spawn_obstacle(object):
+	
 	if(object.type==0):
+		
 		var newBoulder = boulder.instantiate()
 		add_child(newBoulder)
 		var spawnPosition=object.position
@@ -78,20 +84,21 @@ func _spawn_obstacle(object):
 		newBoulder.global_position=spawners[spawnPosition.x][spawnPosition.y]
 		
 		var direction = Globals.directions.RIGHT
-		if(spawnPosition.x==6&&spawnPosition.y>0):
+		
+		if(spawnPosition.x == 6 and spawnPosition.y > 0):
 			direction = Globals.directions.LEFT
 			newBoulder.global_position.x+=256
-		elif(spawnPosition.x>0&&spawnPosition.y==6):
+		elif(spawnPosition.x > 0 and spawnPosition.y == 6):
 			direction = Globals.directions.UP
 			newBoulder.global_position.y+=256
-		elif(spawnPosition.x>0&&spawnPosition.y==0):
+		elif(spawnPosition.x > 0 and spawnPosition.y == 0):
 			direction = Globals.directions.DOWN
 		
 		newBoulder.direction=direction
 		
 		var SFX = newBoulder.find_child("BoulderSFX")
 		SFX.play()
-		object.activated=true
+		object.activated = true
 		print("spawned boulder!")
 
 '''
